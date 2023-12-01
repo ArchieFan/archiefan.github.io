811c9f435d30c4a07640dd83b7afd18f42a3ccdd
Archie Fan <archiefan@gmail.com>
2023-12-01 16:14:03 +0000

    Amended layout

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,21 +1,47 @@
 
 // Fetch and insert header content
 fetch('header.html')
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
     .then(html => {
         document.getElementById('header-content').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
     });
 
 // Fetch and insert banner content
 fetch('banner.html')
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
     .then(html => {
         document.getElementById('banner-content').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
     });
 
+
 // Fetch and insert footer content
+
 fetch('footer.html')
-    .then(response => response.text())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
     .then(html => {
         document.getElementById('footer-content').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
     });
